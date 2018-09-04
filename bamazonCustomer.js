4c744c431844c2930915912792935f24ec00f9c7
Johnny Zavala <jzavalafgz@gmail.com>
2018-09-03 22:48:01 -0400

    pulled data from database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,6 +15,15 @@ let connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    queryAllProducts();
     connection.end();
-    // runSearch();
 });
+
+function queryAllProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        // console.log(res);
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].product_name + " | " + res[i].department_name + " | $" + res[i].price + " | " + res[i].stock_quantity);
+        }
+    });
+}

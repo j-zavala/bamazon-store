d3c38257605f77dc9c57b9dc9902721c250a88fd
Johnny Zavala <jzavalafgz@gmail.com>
2018-09-08 18:28:11 -0400

    Customerjs now works

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,62 +18,87 @@ function queryAllProducts() {
             console.log("Item ID: " + res[i].item_id + "   //   " + "Product: " + res[i].product_name + "   //   " + "Department: " + res[i].department_name + "   //   " + "Price: $" + res[i].price + "   //   " + "QTY: " + res[i].stock_quantity);
         }
         console.log("–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––");
+
+        promptUserPurchase();
     });
 }
 
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    queryAllProducts();
-    start();
-    connection.end();
-});
+//ensures the users is only inputting positive integers
+function validateInput(value) {
+    var integer = Number.isInteger(parseFloat(value));
+    var sign = Math.sign(value);
 
-console.log("*  *   *   *   *   *   *   *");
+    if (integer && (sign === 1)) {
+        return true;
+    } else {
+        return 'Please enter a whole non-zero number.';
+    }
+}
 
-function start() {
-    inquirer
-        .prompt({
-            name: "buyOrView",
-            type: "rawlist",
-            message: "Would you like to [BUY] an item or [VIEW] an item?",
-            choices: ["BUY", "VIEW"]
-        })
-        .then(function (answer) {
-            //based on their answer, either call the bid or the post functions
-            if (answer.buyOrView.toUpperCase() === "BUY") {
-                buyItem();
+function promptUserPurchase() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'item_id',
+            message: 'Please enter the ID of the item you would like to purchase.',
+            validate: validateInput,
+            filter: Number
+        },
+        {
+            type: 'input',
+            name: 'quantity',
+            message: 'How many do you need?',
+            validate: validateInput,
+            filter: Number
+        }
+    ]).then(function (input) {
+        var item = input.item_id;
+        var quantity = input.quantity;
+
+        var queryStr = 'SELECT * FROM products WHERE ?';
+
+        connection.query(queryStr, { item_id: item }, function (err, data) {
+            if (err) throw err;
+
+            if (data.length === 0) {
+                console.log('ERROR: Invalid Item ID. Please select a valid Item ID.');
+                queryAllProducts();
             }
             else {
-                viewItem();
-            }
-        });
-}
+                var productData = data[0];
 
-function buyItem() {
-}
+                if (quantity <= productData.stock_quantity) {
+                    console.log('Congrats! The product you requested is in stock! Placing order!');
 
+                    var updateQueryStr = 'UPDATE products SET stock_quantity =  ' + (productData.stock_quantity - quantity) + ' WHERE item_id= ' + item;
 
+                    connection.query(updateQueryStr, function (err, data) {
+                        if (err) throw err;
 
+                        console.log('Your order has been placed! Your total is $' + productData.price * quantity);
+                        console.log('Thank you for shopping with us!');
+                        console.log("–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––");
 
-function viewItem() {
-    //prompt the user the id of the item they want to buy
-    inquirer
-        .prompt([
-            {
-                name: "id",
-                type: "input",
-                message: "What is the ID of the item you would like to buy?"
-            }
-        ])
-        .then(function (answer) {
-            connection.query("SELECT * FROM bamazon WHERE ?", { id: answer.item_id }, function (err, res) {
-                for (var i = 0; i < res.length; i++) {
-                    console.log("Item ID: " + res[i].item_id + "   //   " + "Product: " + res[i].product_name + "   //   " + "Department: " + res[i].department_name + "   //   " + "Price: $" + res[i].price + "   //   " + "QTY: " + res[i].stock_quantity);
+                        connection.end();
+                    })
+                } else {
+                    console.log('Sorry, there is not enough product in stock, your order can not be placed as is.');
+                    console.log('Please modify your order.');
+                    console.log("–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––");
+
+                    queryAllProducts();
+
                 }
-                console.log("–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––");
-            });
-        });
+
+            }
+        })
+    })
 }
 
+// runBamazon will execute the main application logic
+function runBamazon() {
 
+    queryAllProducts();
+}
+
+runBamazon();
